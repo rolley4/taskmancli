cdd78099ae9ead8da63616ac0498e7eabf82061b
Rolley Bateman <rcbiv@yahoo.com>
2018-07-16 15:27:12 -0500

    Getting started

--- taskmancli.pl
+++ taskmancli.pl
@@ -1,1 +1,19 @@
 #/usr/bin/perl
+
+use strict;
+use Getopt::Std;
+
+my %opts;
+getopts('hval'. \%opts);
+
+my $help      = $opts{'h'};
+my $verbose   = $opts{'v'};
+my $addTasks  = $opts{'a'};
+
+sub syntax{
+  # Disaply Syntax and Exit
+  #TODO
+  my $sysntax = "Usage $0 something"
+  print $sysntax;
+  exit;
+}
