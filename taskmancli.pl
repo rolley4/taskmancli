e9a906f26674611426f3bf6b00cde782bfc2cd4d
Rolley Bateman <rcbiv@yahoo.com>
2018-07-17 14:14:21 -0500

    add list

--- taskmancli.pl
+++ taskmancli.pl
@@ -6,12 +6,13 @@ use LWP;
 use Getopt::Std;
 
 my %opts;
-getopts('hva:d', \%opts);
+getopts('hva:dl', \%opts);
 
-my $help      = $opts{'h'};
-my $verbose   = $opts{'v'};
-my $addTasks  = $opts{'a'};
-my $dueDate   = $opts{'d'};
+my $help        = $opts{'h'};
+my $verbose     = $opts{'v'};
+my $addTasks    = $opts{'a'};
+my $dueDate     = $opts{'d'};
+my $listTasks   = $opts{'l'};
 
 sub syntax {
   # Disaply Syntax and Exit
@@ -28,4 +29,12 @@ if ($addTasks) {
     or die "Could not open file '$filename' $!";
 
   print $fh "$addTasks";
+}
+
+if ($listTasks) {
+  my @files = <TASKS/*.tsk>;
+
+  foreach my $task (@files) {
+    print $task . "\n";
+  }
 }
