bcaf02bf29a4d516eb2f5ae4bbca5927f781c8c1
Rolley Bateman <rcbiv@yahoo.com>
2018-07-18 07:46:24 -0500

    task editor added

--- taskmancli.pl
+++ taskmancli.pl
@@ -7,7 +7,7 @@ use Getopt::Std;
 use Term::ANSIColor qw( colored );
 
 my %opts;
-getopts('hva:dl', \%opts);
+getopts('hvadl', \%opts);
 
 my $help        = $opts{'h'};
 my $verbose     = $opts{'v'};
@@ -28,15 +28,17 @@ if ($addTasks) {
   my $filename = 'TASKS/' . $taskHash .  '.tsk';
   open(my $fh, '>', $filename)
     or die "Could not open file '$filename' $!";
-
-  print $fh "$addTasks";
+  print $fh "TASK:TASK TITLE\n" . "TASKSTART:" . (localtime($taskHash)) . "\n" . "TASKEND:" . (localtime($taskHash+864000)) . "\n" . "NOTES:";
   close $fh;
+  system('pico', $filename);
 }
 
 if ($listTasks) {
   my @files = <TASKS/*.tsk>;
 
+  my $colorToggle = 0;
   print colored('|---ID---| |-Start Date and Time--| |-Task Name-->', 'white on_blue'), "\n";
+  #print colored('|---ID---| |-Start Date and Time--| |-Task Name-->', 'white on_blue'), "\n";
   foreach my $task (@files) {
     my $taskEpoch;
     ($taskEpoch) = $task =~ /\/(.*)\./;
@@ -44,7 +46,17 @@ if ($listTasks) {
     open(my $fh, '<:encoding(UTF-8)', $task)
       or die "Could not open file '$task' $!";
 
+    my $listColor;
+    if ($colorToggle) {
+      $colorToggle = 0;
+      $listColor = 'green';
+    } else {
+      $colorToggle = 1;
+      $listColor = 'blue';
+    }
     my @taskContent = <$fh>;
-    print $taskEpoch . " " . localtime($taskEpoch) . " " . $taskContent[0] . "\n";
+    chomp($taskContent[0]);
+    my $taskDetailOut = $taskEpoch . " " . localtime($taskEpoch) . " " . $taskContent[0];
+    print colored($taskDetailOut, $listColor) . "\n";
   }
 }
