a00afb9d4a333255e34f8054c86b1f6f6ad0ce5d
Rolley Bateman <rcbiv@yahoo.com>
2018-07-18 09:59:37 -0500

    looking good

--- taskmancli.pl
+++ taskmancli.pl
@@ -12,7 +12,7 @@ getopts('hvadl', \%opts);
 my $help        = $opts{'h'};
 my $verbose     = $opts{'v'};
 my $addTasks    = $opts{'a'};
-my $dueDate     = $opts{'d'};
+my $details     = $opts{'d'};
 my $listTasks   = $opts{'l'};
 
 sub syntax {
@@ -23,6 +23,7 @@ sub syntax {
   exit;
 }
 
+# ADD NEW TASKS
 if ($addTasks) {
   my $taskHash = time();
   my $filename = 'TASKS/' . $taskHash .  '.tsk';
@@ -33,18 +34,45 @@ if ($addTasks) {
   system('pico', $filename);
 }
 
+# LIST TASKS
 if ($listTasks) {
   my @files = <TASKS/*.tsk>;
 
   my $colorToggle = 0;
-  print colored('|---ID---| |-Start Date and Time--| |-Task Name-->', 'white on_blue'), "\n";
+  print colored('|---ID----| |---Start Date and Time--| |--_-End Date and Time---| |--------Task----------->', 'white on_blue'), "\n";
   #print colored('|---ID---| |-Start Date and Time--| |-Task Name-->', 'white on_blue'), "\n";
   foreach my $task (@files) {
-    my $taskEpoch;
-    ($taskEpoch) = $task =~ /\/(.*)\./;
+    my ($taskEpoch) = $task =~ /\/(.*)\./;
+    my $taskContent;
+    my $taskItemStart;
+    my $taskItemEnd;
+    my @taskNotes;
 
     open(my $fh, '<:encoding(UTF-8)', $task)
       or die "Could not open file '$task' $!";
+
+    while (my $row = <$fh>) {
+      if ($row =~ /^TASK\:.*/) {
+        chomp $row;
+        $row =~ s/^TASK\://;
+        $taskContent = $row;
+      } elsif ($row =~ /^TASKSTART\:.*/) {
+        chomp $row;
+        $row =~ s/^TASKSTART\://;
+        $taskItemStart = $row;
+      } elsif ($row =~ /^TASKEND\:.*/) {
+        chomp $row;
+        $row =~ s/^TASKEND\://;
+        $taskItemEnd = $row;
+      } elsif ($row =~ /^NOTES\:.*/) {
+        chomp $row;
+        $row =~ s/^NOTES\://;
+        push @taskNotes, $row;
+      } else {
+        chomp $row;
+        push @taskNotes, $row;
+      }
+    }
 
     my $listColor;
     if ($colorToggle) {
@@ -54,9 +82,36 @@ if ($listTasks) {
       $colorToggle = 1;
       $listColor = 'blue';
     }
-    my @taskContent = <$fh>;
-    chomp($taskContent[0]);
-    my $taskDetailOut = $taskEpoch . " " . localtime($taskEpoch) . " " . $taskContent[0];
-    print colored($taskDetailOut, $listColor) . "\n";
+
+    print colored($taskEpoch, $listColor) . "| |" . colored($taskItemStart, $listColor) . "| |" . colored($taskItemEnd, 'red') . "| |" . colored($taskContent, $listColor) . "\n";
+
+    if ($details) {
+      foreach (@taskNotes) {
+        print "\t", colored($_, 'cyan') . "\n";
+      }
+    }
+
+  }
+}
+
+sub listTaskDetails {
+  my $fh;
+  while (my $row = <$fh>) {
+    if ($row =~ /^TASK\:.*/) {
+      chomp $row;
+      print "Found Tasks " . $row . "\n";
+    } elsif ($row =~ /^TASKSTART\:.*/) {
+      chomp $row;
+      print "Found Task start" . $row . "\n";
+    } elsif ($row =~ /^TASKEND\:.*/) {
+      chomp $row;
+      print "Found Task end " . $row . "\n";
+    } elsif ($row =~ /^NOTES\:.*/) {
+      chomp $row;
+      print "Found Task notes " . $row . "\n";
+    } else {
+      chomp $row;
+      print "notes - " . $row . "\n";
+    }
   }
 }
